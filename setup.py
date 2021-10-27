aef6fe774884e7e02ef652b60fc8b39fdcad9f80
Cameron Wong <cw@reosource.io>
2021-10-27 16:22:36 -0700

    Root commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rlog",
+    version="21.06.2",
+    author="Cameron Wong",
+    description="personal python logging configuration (console + rotating file log)",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["python-decouple"],
+)
---
+++ src/rlog/__init__.py
@@ -0,0 +1,47 @@
+import logging
+import logging.handlers
+
+from decouple import config
+
+# file paths
+log_file = config("LOG_FILE", default="/tmp/reo.log")
+base = log_file.split(".")[0]
+err_file = ".".join([base, "err"])
+
+# formatter
+formatter = logging.Formatter("%(asctime)s %(levelname)s [%(filename)s.%(funcName)s]: %(message)s")
+
+# normal log file
+logfile_handler = logging.handlers.RotatingFileHandler(
+    log_file, maxBytes=104857600, backupCount=9
+)
+logfile_handler.setLevel(logging.DEBUG)
+logfile_handler.setFormatter(formatter)
+
+# err log file
+errfile_handler = logging.handlers.RotatingFileHandler(
+    err_file, maxBytes=104857600, backupCount=9
+)
+errfile_handler.setLevel(logging.WARNING)
+errfile_handler.setFormatter(formatter)
+
+# console log stream
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+
+# register handlers
+logging.getLogger().addHandler(console)
+logging.getLogger().addHandler(logfile_handler)
+logging.getLogger().addHandler(errfile_handler)
+logging.getLogger().setLevel(logging.DEBUG)
+
+# initial message
+logging.info("Registered logging framework")
+
+# log methods
+debug = logging.debug
+info = logging.info
+warning = logging.warning
+error = logging.error
+critical = logging.critical
